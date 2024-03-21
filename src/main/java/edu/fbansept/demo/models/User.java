ba164c1fb27296a769fa8aad8bbfc7e4abf9530a
fbansept <bansept.franck@gmail.com>
2024-03-21 14:23:19 +0100

    prepare security

---
+++ src/main/java/edu/fbansept/demo/models/User.java
@@ -0,0 +1,21 @@
+package edu.fbansept.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Integer id;
+    protected String email;
+    protected String password;
+
+}
---
+++ src/main/java/edu/fbansept/demo/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package edu.fbansept.demo.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    AppUserDetailsService userDetailsservice;
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(userDetailsservice);
+        return daoAuthenticationProvider;
+    }
+
+}
